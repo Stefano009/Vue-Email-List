487932bde431963807919dea3381a7cf70a9313a
Stefano <stefano.funaro@gmail.com>
2021-09-16 16:26:05 +0200

    show-email-just-when-generated-all-10

---
+++ Bonus/js/main.js
@@ -0,0 +1,24 @@
+Vue.config.devtools = true;
+
+const app = new Vue({
+    el: '#root',
+    data: {
+        emailGenerated: [],
+    },
+    mounted() {
+        this.emailGenerator()
+    },
+    methods: {
+        emailGenerator() {
+            for (i = 0; i < 10; i++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                    .then(function(obj) {
+                        console.log(obj.data);
+                        //il mio console.log mi da ogni volta una mail diversa, quindi abbiamo un generatore di mail
+                        app.emailGenerated.push(obj.data.response);
+                        console.log(app.emailGenerated)
+                    })
+            }
+        }
+    }
+})
